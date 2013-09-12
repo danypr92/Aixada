41184d4937ab809f30040444507423b33b5b04db
ismael <ismael.bartolome@gmail.com>
2013-09-12 20:36:34 +0200

    Dirección cooperativa

--- tpl/incidents_model1.php
+++ tpl/incidents_model1.php
@@ -86,11 +86,11 @@
 			<img id="coopLogo" alt="coop logo" src="../img/tpl_header_logo.png" width="500" height="180"/>
 		</div>
 		<div id="address">
-			<h2 class="txtAlignRight">COOPERATIVA NAME</h2>
-			<h2 class="txtAlignRight">CIF/NIF: F650000</h2>
-			<p class="txtAlignRight">Street<br/>
-			Zip City<br/>
-			email@bla.com
+			<h2 class="txtAlignRight">COOPERATIVA Gira-sol de Sant Martí</h2>
+			<h2 class="txtAlignRight">-</h2>
+			<p class="txtAlignRight">C/ Doctor Zamenhof, 23<br/>
+			Barcelona<br/>
+			cooperativa-gira-sol-de-sant-marti@googlegroups.com
 			</p>
 		</div>
 	</div>
